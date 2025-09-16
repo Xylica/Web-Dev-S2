1733f9a5ff021965ac91771cbf11333b64770dde
Catherine Gizzi (1000099545) <GIZZCM1@student.op.ac.nz>
2025-09-17 09:26:05 +1200

    Starting first svelte kit project to learn in

---
+++ Learning-Svelte/Learning-Svelte/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>{title}</h1>
+<p>{"[ Visit"} <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> {"to read the documentation ]"}</p>
+<script>
+    let title = $state("[ Home ]");
+</script>
+<p>{"[ Welcome to my Home Page ]"}</p>
+<nav>
+    <a href = "/">| Home |</a>
+    <a href = "/about">| About |</a>
+    <a href = "/content">| Content |</a>
+</nav>
